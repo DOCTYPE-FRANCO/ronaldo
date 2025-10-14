e0416b8ca5acbb2ca8b9b84c65ae3c96c4aceb35
Franco <ufrancis186@gmail.com>
2025-10-14 16:43:13 +0100

    collect this one first

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
         <div
           className="absolute inset-0 z-0"
           style={{
-            background: "radial-gradient(125% 125% at 50% 100%, #000000 40%, #2b0707 100%)",
+            background: "radial-gradient(125% 125% at 50% 10%, #000000 40%, #2b0707 100%)",
           }}
         />
         {/* Your Content/Components */}
@@ -30,3 +30,5 @@ function App() {
 }
 
 export default App
+
+
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import CR7 from "../assets/CR7LOGO.png"
+
 
 function Header(){
     return(
-        <div>
-            
+        <div className="top-4 fixed flex justify-self-center justify-between z-50 w-[95vw]  backdrop-blur-sm rounded-sm ">
+            <img src={CR7} className="w-[80px] "/>
         </div>
     );
 }
