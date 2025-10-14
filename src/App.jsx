a36d89ebc32315d8857352049fbb1d10656e649d
Franco <ufrancis186@gmail.com>
2025-10-14 16:09:24 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import React from "react"
+import { Routes, Route } from "react-router-dom"
+import Homepage from "./Components/Homepage"
+
+function App() {
+  return (
+    
+      <div className="min-h-screen w-full relative">
+        {/* Crimson Depth */}
+        <div
+          className="absolute inset-0 z-0"
+          style={{
+            background: "radial-gradient(125% 125% at 50% 100%, #000000 40%, #2b0707 100%)",
+          }}
+        />
+        {/* Your Content/Components */}
+
+        <div className="z-10">
+          <div>
+            <Routes>
+              <Route path="/" element={<Homepage/>}/>
+            </Routes>
+          </div>
+        </div>
+      </div>
+    
+  )
+}
+
+export default App
